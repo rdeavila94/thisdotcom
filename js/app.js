887904a1bdae7633da127a763100d07373d72859
Richard <crayonsrcool@hotmail.com>
2019-10-17 00:12:36 -0700

    Added fade-in animation abilities for when an element is scrolled into

--- js/app.js
+++ js/app.js
@@ -13,56 +13,63 @@
 //     }, 100 * i);
 // }
 
-//how quickly text should be deleted/written
-const writeSpeed = 10;
+// how quickly text should be deleted/written
+const writeSpeed = 10
 
-const terminalTextCounter = 0;
+const terminalText = document.querySelector('#terminal-text')
 
-const terminalText = document.querySelector('#terminal-text');
+const terminalParent = terminalText.parentElement
 
-const terminalParent = terminalText.parentElement;
+const elements = document.querySelectorAll('.composition__image img')
 
-const terminalAppear = document.querySelector('#terminal-appear');
-
-const elements = document.querySelectorAll('.composition__image img');
-
+const sectionAbout = document.querySelector('.section-about')
 
 const altMap = {
-    "csulb": "My alma mater: beautiful Long Beach State. Where I wrote my first (but certainly not last) 'Hello, World!'",
-    "la": "The city of Angeles. Glamorous. Fruitful. Challenging. The skyline whose shadow I was raised under. Not for the faint of heart but rewarding to those who conquer it.",
-    "mayan": "The Mayan nightclub. Want to find on a Saturday night? Meet me on the dance floor."
+  csulb: 'My alma mater: beautiful Long Beach State. Where I wrote my first (but certainly not last) "Hello, World!"',
+  la: 'The city of Angeles. Glamorous. Fruitful. Challenging. The skyline whose shadow I was raised under. Not for the faint of heart but rewarding to those who conquer it.',
+  mayan: 'The Mayan nightclub. Want to find on a Saturday night? Meet me on the dance floor.'
 }
 
 const rewriteText = alt => {
-    if (terminalText.textContent) {
-        removeText(() => writeText(altMap[alt]));
-    }
-    writeText(altMap[alt]);
+  if (terminalText.textContent) {
+    removeText(() => writeText(altMap[alt]))
+  }
+  writeText(altMap[alt])
 }
 
 const writeText = (message, terminal) => {
-    for (let i = 0; i < message.length; i++) {
-        setTimeout(() => {
-            terminal.textContent = terminal.textContent + message.charAt(i);
-        }, writeSpeed * i);
-    }
+  for (let i = 0; i < message.length; i++) {
+    setTimeout(() => {
+      terminal.textContent = terminal.textContent + message.charAt(i)
+    }, writeSpeed * i)
+  }
 }
 
 const removeText = (callback) => {
-    const textLength = terminalText.textContent.length;
-    for (let i = 0; i <= textLength; i++) {
-        setTimeout(() => {
-            terminalText.textContent = terminalText.textContent.slice(0, textLength - i);
-        }, writeSpeed * i);
-    }
-    setTimeout(callback, writeSpeed * (textLength + 1));
+  const textLength = terminalText.textContent.length
+  for (let i = 0; i <= textLength; i++) {
+    setTimeout(() => {
+      terminalText.textContent = terminalText.textContent.slice(0, textLength - i)
+    }, writeSpeed * i)
+  }
+  setTimeout(callback, writeSpeed * (textLength + 1))
 }
 
 elements.forEach(element => element.addEventListener('click', event => {
-    terminalParent.removeChild(document.querySelector('#terminal-text'));
-    const p = document.createElement('p');
-    p.className = 'composition__terminal__text';
-    p.id = 'terminal-text';
-    terminalParent.appendChild(p);
-    writeText(altMap[event.srcElement.alt], p);
-}));
+  terminalParent.removeChild(document.querySelector('#terminal-text'))
+  const p = document.createElement('p')
+  p.className = 'composition__terminal__text'
+  p.id = 'terminal-text'
+  terminalParent.appendChild(p)
+  writeText(altMap[event.srcElement.alt], p)
+}))
+
+// remove this event listener once they've scrolled all the way down
+document.addEventListener('scroll', e => {
+  const windowHeight = window.innerHeight
+  const windowDistance = window.pageYOffset
+
+  if (sectionAbout.offsetHeight > windowHeight - windowDistance) {
+    sectionAbout.className = 'section-about fade-in'
+  }
+})
