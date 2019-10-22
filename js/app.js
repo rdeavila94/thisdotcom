b251ea184aff37a6fabd96ccf8eadfb3e0c61778
Richard <crayonsrcool@hotmail.com>
2019-10-21 23:13:52 -0700

    Completed the typing animation

--- js/app.js
+++ js/app.js
@@ -1,26 +1,41 @@
-function AnimatedParent(element, animatedChildren) {
+// Constructors for objects to facilitate animations nested within a fade in section
+function AnimatedParent (element, animatedChildren) {
   this.element = element
   this.animatedChildren = animatedChildren
 }
 
-function AnimatedChild(element, animationName) {
+function AnimatedChild (element, animationName) {
   this.element = element
   this.animationName = ' ' + animationName
 }
 
+// To avoid repeating the trigger on subsequent scrolls that satisfy the trigger
 let aboutTriggered = false
 let technicalTriggered = false
 
+// How far into sections to trigger the fade in
 const aboutOffsetMargin = 200
 const technicalOffsetMargin = 400
+
+// How long the fade in animation is configured to last on the CSS side
+const fadeInAnimationDelay = 500
 
 const animatedLine = new AnimatedChild(document.querySelector('#header-line'), 'u-line--animated')
 const sectionAbout = new AnimatedParent(document.querySelector('#section-about'), [animatedLine])
 
-const underscore = document.querySelector('#underscore')
+const typeAnimate = document.querySelector('#type-animate')
 
 const animatedHr = new AnimatedChild(document.querySelector('#animated-hr'), 'u-line-slice--animated')
 const sectionTechnical = new AnimatedParent(document.querySelector('#section-technical'), [animatedHr])
+
+// Function to create a 'typing' animation
+const writeText = (message, writeSpeed = 100, element = typeAnimate) => {
+  for (let i = 0; i < message.length; i++) {
+    setTimeout(() => {
+      element.textContent += message.charAt(i)
+    }, writeSpeed * i)
+  }
+}
 
 // remove this event listener once they've scrolled all the way down
 document.addEventListener('scroll', e => {
@@ -40,6 +55,7 @@ document.addEventListener('scroll', e => {
     sectionTechnical.animatedChildren.forEach(child => {
       child.element.className += child.animationName
     })
+    setTimeout(() => writeText('Java + Node'), fadeInAnimationDelay * 2)
     technicalTriggered = true
   }
 })
