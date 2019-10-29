16682ff455a7b437a748c39c685347894b2cfc32
Richard <crayonsrcool@hotmail.com>
2019-10-29 00:24:36 -0700

    Revert "Changed the animation for the name greeting in bio"
    This reverts commit 79a847e5155423096aa210d5d812207601e4e8f5.

--- js/app.js
+++ js/app.js
@@ -33,18 +33,12 @@ const java = new AnimatedChild(document.querySelector('#java'), 'title-box__anim
 const javascript = new AnimatedChild(document.querySelector('#javascript'), 'title-box__animate-right')
 const sectionTechnical = new AnimatedParent(document.querySelector('#section-technical'), [animatedHr, popupHeader, java, javascript])
 
-const name = document.querySelector('#name')
-const greeting = document.querySelector('#greeting')
-
 // Function to create a 'typing' animation
-const writeText = (message, writeSpeed = 100, element = typeAnimate, callback) => {
+const writeText = (message, writeSpeed = 100, element = typeAnimate) => {
   for (let i = 0; i < message.length; i++) {
     setTimeout(() => {
       element.textContent += message.charAt(i)
     }, writeSpeed * i)
-  }
-  if (callback) {
-    setTimeout(callback, writeSpeed * message.length)
   }
 }
 
@@ -58,7 +52,6 @@ const onscroll = e => {
     sectionAbout.animatedChildren.forEach(child => {
       child.element.className += child.animationName
     })
-    setTimeout(() => writeText("Hi, I'm", 100, greeting, () => writeText('Richard', 100, name)), fadeInAnimationDelay)
     aboutTriggered = true
   }
 
