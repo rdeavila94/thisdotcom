067a5447eb0a6e0ef627ae748d99f3b42cecedf8
Richard <crayonsrcool@hotmail.com>
2019-10-30 23:34:01 -0700

    Drastically improved the framework for animating elements when the screen displays them. the top of getBoundingClientRect is compared against the window height to see if its less than(indicating its within the windows view)

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
-// Constructors for objects to facilitate animations nested within a fade in section
-function AnimatedParent(element, animatedChildren) {
+function AnimatedElement(element, animationClass, replacingClass = undefined) {
   this.element = element
-  this.animatedChildren = animatedChildren
+  this.animationClass = animationClass
+  this.replacingClass = replacingClass
 }
 
-function AnimatedChild(element, animationName) {
-  this.element = element
-  this.animationName = ' ' + animationName
+AnimatedElement.prototype.animate = function () {
+  this.replacingClass ? this.element.classList.replace(this.replacingClass, this.animationClass) : this.element.classList.add(this.animationClass)
 }
+
+let windowHeight = window.innerHeight
 
 // To avoid repeating the trigger on subsequent scrolls that satisfy the trigger
 let greetingTriggered = false
@@ -16,18 +17,17 @@ let compTriggered = false
 let technicalTriggered = false
 let cardTriggered = false
 
-const greeting = new AnimatedChild(document.querySelector('#greeting'), 'come-in-left')
-const name = new AnimatedChild(document.querySelector('#name'), 'u-flip-in')
-const animatedLine = new AnimatedChild(document.querySelector('#header-line'), 'u-line--animated')
-const sectionAbout = new AnimatedParent(document.querySelector('#section-about'), [animatedLine, greeting, name])
-
-const typeAnimate = document.querySelector('#type-animate')
-
-const animatedHr = new AnimatedChild(document.querySelector('#animated-hr'), 'title-box__line--animated')
-const popupHeader = new AnimatedChild(document.querySelector('#popup-header'), 'pop-up')
-const java = new AnimatedChild(document.querySelector('#java'), 'title-box__animate-left')
-const javascript = new AnimatedChild(document.querySelector('#javascript'), 'title-box__animate-right')
-const sectionTechnical = new AnimatedParent(document.querySelector('#section-technical'), [animatedHr, popupHeader, java, javascript])
+const greeting = new AnimatedElement(document.querySelector('#greeting'), 'come-in-left', 'hidden')
+const name = new AnimatedElement(document.querySelector('#name'), 'u-flip-in', 'hidden')
+const uLine = new AnimatedElement(document.querySelector('#header-line'), 'u-line--animated', 'hidden')
+const bioCol = new AnimatedElement(document.querySelector('#bio-col'), 'col-fade-in-left', 'hidden')
+const compCol = new AnimatedElement(document.querySelector('#comp-col'), 'col-fade-in-right', 'hidden')
+const animatedHr = new AnimatedElement(document.querySelector('#animated-hr'), 'title-box__line--animated', 'hidden')
+const popupHeader = new AnimatedElement(document.querySelector('#popup-header'), 'pop-up', 'hidden')
+const java = new AnimatedElement(document.querySelector('#java'), 'title-box__animate-left', 'hidden')
+const javascript = new AnimatedElement(document.querySelector('#javascript'), 'title-box__animate-right', 'hidden')
+const card1 = new AnimatedElement(document.querySelector('#col-card-1'), 'col-fade-in-left', 'hidden')
+const card2 = new AnimatedElement(document.querySelector('#col-card-2'), 'col-fade-in-right', 'hidden')
 
 // Function to create a 'typing' animation
 const writeText = (message, writeSpeed = 100, element = typeAnimate) => {
@@ -38,67 +38,47 @@ const writeText = (message, writeSpeed = 100, element = typeAnimate) => {
   }
 }
 
-const bio = new AnimatedChild(document.querySelector('#bio-col'), 'col-fade-in-left')
-const comp = new AnimatedChild(document.querySelector('#comp-col'), 'col-fade-in-right')
-
-let offsetTrigger, sectionHeaderHeight, sectionAboutHeight, greetingTrigger, bioTrigger, compTrigger, technicalTrigger, cardTrigger
-
-// How far into sections to trigger the fade in
-const calculateOffsets = () => {
-  offsetTrigger = 100
-  sectionHeaderHeight = document.querySelector('.section-header').offsetHeight
-  sectionAboutHeight = document.querySelector('.section-about').offsetHeight + sectionHeaderHeight
-  greetingTrigger = sectionHeaderHeight + offsetTrigger
-  bioTrigger = greetingTrigger + document.querySelector('#greeting-row').offsetHeight
-  compTrigger = bioTrigger + comp.element.offsetHeight
-  technicalTrigger = sectionAboutHeight + offsetTrigger
-  cardTrigger = document.querySelector('body').offsetHeight - document.querySelector('.footer').offsetHeight - document.querySelector('#card-row').offsetHeight
-}
-calculateOffsets()
-
-const card1 = new AnimatedChild(document.querySelector('#col-card-1'), 'col-fade-in-left')
-const card2 = new AnimatedChild(document.querySelector('#col-card-2'), 'col-fade-in-right')
-
 // We have to use a named function in order to have the ability to remove the handler
 const onscroll = e => {
-  const windowHeight = window.innerHeight
-  const windowDistance = window.pageYOffset
-
-  if (greetingTrigger < windowHeight + windowDistance - offsetTrigger && !greetingTriggered) {
+  if (!greetingTriggered && greeting.element.getBoundingClientRect().top < windowHeight) {
     greetingTriggered = true
-    sectionAbout.animatedChildren.forEach(child => {
-      child.element.className += child.animationName
-    })
+    greeting.animate()
+    name.animate()
+    uLine.animate()
   }
 
-  if (bioTrigger < windowHeight + windowDistance - offsetTrigger && !bioTriggered) {
+  if (greetingTriggered && !bioTriggered && bioCol.element.getBoundingClientRect().top < windowHeight) {
     bioTriggered = true
-    bio.element.className = 'col-6 ' + bio.animationName
+    bioCol.animate()
   }
 
-  if (compTrigger < windowHeight + windowDistance - offsetTrigger && !compTriggered) {
+  if (bioTriggered && !compTriggered && compCol.element.getBoundingClientRect().top < windowHeight - 100) {
     compTriggered = true
-    comp.element.className = 'col-6 ' + comp.animationName
+    compCol.animate()
   }
 
-  if (technicalTrigger < windowHeight + windowDistance - offsetTrigger && !technicalTriggered) {
+  if (compTriggered && !technicalTriggered && popupHeader.element.getBoundingClientRect().top < windowHeight) {
     technicalTriggered = true
-    sectionTechnical.animatedChildren.forEach(child => {
-      child.element.className += child.animationName
-    })
+    animatedHr.animate()
+    popupHeader.animate()
+    java.animate()
+    javascript.animate()
   }
 
-  if (cardTrigger < windowHeight + windowDistance - offsetTrigger && !cardTriggered) {
+  if (technicalTriggered && !cardTriggered && card1.element.getBoundingClientRect().top < windowHeight) {
     cardTriggered = true
-    card1.element.className = 'col-6 ' + card1.animationName
-    card2.element.className = 'col-6 ' + card2.animationName
+    card1.animate()
+    card2.animate()
   }
 
   if (greetingTriggered && bioTriggered && compTriggered && technicalTriggered && cardTriggered) {
     document.removeEventListener('scroll', onscroll)
   }
 }
+
 // remove this event listener once they've scrolled all the way down
 document.addEventListener('scroll', onscroll)
 
-window.addEventListener('resize', calculateOffsets, false)
+window.addEventListener('resize', () => {
+  windowHeight = window.innerHeight
+}, false)
